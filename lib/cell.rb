59cd05128396ca405b88c8be1f674b270e9e4369
Linda Le <yueling1222@gmail.com>
2019-10-07 17:46:55 -0600

    Add place ship method and passing tests to see if cell has a ship.
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,26 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class Cell
   attr_reader :coordinate
+  attr_accessor :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = []
   end
 
+  def ship
+    @ship
+  end
+
+  def empty?
+    @ship == []
+  end
+
+  def place_ship(ship_to_be_placed)
+    @ship << ship_to_be_placed
+  end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -8,6 +8,7 @@ class CellTest < MiniTest::Test
 
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_is_a_cell
@@ -17,4 +18,17 @@ class CellTest < MiniTest::Test
   def test_it_has_coordinate
     assert_equal "B4", @cell.coordinate
   end
+
+  def test_cell_is_empty
+    assert_equal true, @cell.empty?
+  end
+
+  def test_it_has_a_ship
+    assert_equal true, @cell.empty?
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.empty?
+    assert_equal @cruiser, @cell.ship.first
+  end
+
+
 end
