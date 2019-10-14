cd6e3f7cd781988c8b1fe731743da64484a7b6a7
Wren Steitle <lauren.steitle@gmail.com>
2019-10-14 11:00:36 -0600

    Create Player class and tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,12 +1,17 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/ship'
-require './lib/cell'
-require './lib/board'
+require './lib/player'
 require 'pry'
 
 class PlayerTest < MiniTest::Test
+  def test_player_exists
+    player = Player.new("Andy")
+    assert_instance_of Player, player
+  end
 
-
+  def test_player_has_name
+    player = Player.new("Andy")
+    assert_equal "Andy", player.name
+  end 
 end
