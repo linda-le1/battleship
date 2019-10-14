bf836107ab3d3c590a3cba94701ceeb8a63aaa3a
Wren Steitle <lauren.steitle@gmail.com>
2019-10-14 10:09:34 -0600

    Create player class and test file

---
+++ test/player_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require 'pry'
+
+class PlayerTest < MiniTest::Test
+
+
+end
